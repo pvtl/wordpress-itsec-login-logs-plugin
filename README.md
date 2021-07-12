0bbd764d17028df069f51c39b43a859a315fcb82
Matt McNamee <matt@mcnamee.co>
2021-07-12 15:50:26 +1000

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+# iThemes Security - Log Logins
+
+Adds an entry into the iThemes Security (free) logs (notices), for each successful user login.
+
+## Installation
+
+```bash
+# 1. Get it ready (to use a repo outside of packagist)
+composer config repositories.pvtl-itsec-login-logs git https://github.com/pvtl/wordpress-itsec-login-logs-plugin
+
+# 2. Install the Plugin - we want all updates from this major version (while non-breaking)
+composer require "pvtl/itsec-login-logs:~1.0"
+```
+
+## Versioning
+
+_Do not manually create tags_.
+
+Versioning comprises of 2 things:
+
+- Wordpress plugin version
+    - The version number used by Wordpress on the plugins screen (and various other peices of functionality to track the version number)
+    - Controlled in `./itsec-login-logs.php` by `* Version: x.x.x` (line 10)
+- Composer dependency version
+    - The version Composer uses to know which version of the plugin to install
+    - Controlled by Git tags
+
+Versioning for this plugin is automated using a Github Action (`./.github/workflows/version-update.yml`).
+To release a new version, simply change the `* Version: x.x.x` (line 10) in `./itsec-login-logs.php` - the Github Action will take care of the rest.
---
+++ itsec-login-logs.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Plugin Name:  iThemes Security - Log Logins
+ * Plugin URI:   https://github.com/pvtl/wordpress-itsec-login-logs-plugin
+ * Description:  Adds an entry into the iThemes Security (free) logs (notices), for each successful user login.
+ * Author:       Pivotal Agency
+ * Author URI:   https://pvtl.io/
+ * Text Domain:  pvtl-itsec-login-logs
+ * Domain Path:  /languages
+ * Version:      1.0.0
+ * License:      MIT License
+ *
+ * @package      PVTL_ITSEC_Logs
+ */
+
+/**
+ * Log the successful login
+ *
+ * @param null|WP_User|WP_Error $user - the user object if successful.
+ */
+function pvtl_itsec_log_logins( $user ) {
+	// iThemes Security plugin doesn't exist or isn't active, so don't continue.
+	if ( ! class_exists( 'ITSEC_Log' ) ) {
+		return $user;
+	}
+
+	// Login wasn't attempted or wasn't successful - so don't continue.
+	if ( null === $user || is_wp_error( $user ) ) {
+		return $user;
+	}
+
+	// Login was successful - log it.
+	try {
+		ITSEC_Log::add_notice(
+			'Login', // 'user_logging', // Module.
+			'User successfully logged in', // "user-logged-in::user-{$user->id}", // Code.
+			array(
+				'details'  => ITSEC_Lib::get_login_details(),
+				'user'     => $user,
+				'username' => $user->user_login,
+				'user_id'  => $user->id,
+				'SERVER'   => ITSEC_Lib::get_server_snapshot(),
+			),
+			array( 'user_id' => $user->id ) // Overrides.
+		);
+	} catch ( Exception $e ) {
+		// Ignore - likely the iThemes Security plugin has just changed.
+		return $user;
+	}
+
+	// Continue on your merry way after the log was added.
+	return $user;
+}
+
+add_action( 'authenticate', 'pvtl_itsec_log_logins', 99, 1 );
